440c3cb1390810db7c99dc1d9c4933edfa8b1ed1
sergey <kruelskov2@gmail.com>
2022-10-16 13:08:59 +0800

    2labs files was added

---
+++ prog11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, stepen;
+            Console.WriteLine("Введите число А: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число B: ");
+            b = int.Parse(Console.ReadLine());
+            stepen = 1;
+            for(int i=0; i<b; i++)
+            {
+                stepen *= a;
+            }
+            Console.WriteLine("Число {0} в степени {1} = {2}",a,b,stepen);
+        }
+    }
+}
---
+++ prog16/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            double raznost, max, min;
+            max = 0;
+            min = 100000;
+            Console.WriteLine("Введите размер массива: ");
+            n = int.Parse(Console.ReadLine());
+            double[] mas = new double[n];
+            Console.WriteLine("Введите элементы массива: ");
+            for (int i=0; i < n; i++)
+            {
+                mas[i] = double.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (mas[i] > max)
+                {
+                    max = mas[i];
+                }
+                if (mas[i] < min)
+                {
+                    min = mas[i];
+                }
+            }
+            raznost = max - min;
+            Console.WriteLine("Разность максимального - {0} и минимального - {1} элементов массива = {2}",max,min,raznost);
+        }
+    }
+}
